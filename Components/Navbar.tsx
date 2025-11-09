eae8b41300c66664afd1c5ba50597f4812fdc7cb
Wahyu Pratama <ayuramatama@gmail.com>
2025-11-09 13:33:58 +0700

    refactor: simplify Navbar component and integrate useSession for authentication

--- Components/Navbar.tsx
+++ Components/Navbar.tsx
@@ -1,14 +1,75 @@
-import { auth } from "@/auth";
-import { cookies } from "next/headers";
-import { Suspense } from "react";
+// import { auth } from "@/auth";
+// import { cookies } from "next/headers";
+// import { Suspense } from "react";
+// import Image from "next/image";
+// import Link from "next/link";
+// import LogoutLink from "./LogOutLink";
+
+// async function Navbar() {
+//   // check if user is logged in
+//   cookies();
+//   const session = await auth();
+
+//   return (
+//     <header>
+//       <nav>
+//         <Link href="/" className="logo">
+//           <Image src="/icons/logo.png" alt="logo" width={24} height={24} />
+//           <p>DevEvents</p>
+//         </Link>
+
+//         <ul>
+//           <Link href="/">Home</Link>
+//           <Link href="/dashboard">Dashboard</Link>
+//           <Link href="/dashboard/create-event" prefetch={false}>
+//             Create Event
+//           </Link>
+//           {/* use an external component bcoz to clear cookies as client component */}
+//           {session && <LogoutLink userName={session.user.name} />}
+//         </ul>
+//       </nav>
+//     </header>
+//   );
+// }
+
+// function SuspenseWrapper() {
+//   return (
+//     <Suspense
+//       fallback={
+//         <header>
+//           <nav>
+//             <Link href="/" className="logo">
+//               <Image src="/icons/logo.png" alt="logo" width={24} height={24} />
+//               <p>DevEvents</p>
+//             </Link>
+
+//             <ul>
+//               <Link href="/">Home</Link>
+//               <Link href="/dashboard">Dashboard</Link>
+//               <Link href="/dashboard/create-event" prefetch={false}>
+//                 Create Event
+//               </Link>
+//             </ul>
+//           </nav>
+//         </header>
+//       }
+//     >
+//       <Navbar />
+//     </Suspense>
+//   );
+// }
+
+// export default SuspenseWrapper;
+
+
+"use client";
+import { useSession } from "next-auth/react";
 import Image from "next/image";
 import Link from "next/link";
 import LogoutLink from "./LogOutLink";
 
-async function Navbar() {
-  // check if user is logged in
-  cookies();
-  const session = await auth();
+export default function Navbar() {
+  const { data: session } = useSession();
 
   return (
     <header>
@@ -24,39 +85,9 @@ async function Navbar() {
           <Link href="/dashboard/create-event" prefetch={false}>
             Create Event
           </Link>
-          {/* use an external component bcoz to clear cookies as client component */}
-          {session && <LogoutLink userName={session.user.name} />}
+          {session && <LogoutLink userName={session.user?.name} />}
         </ul>
       </nav>
     </header>
   );
 }
-
-function SuspenseWrapper() {
-  return (
-    <Suspense
-      fallback={
-        <header>
-          <nav>
-            <Link href="/" className="logo">
-              <Image src="/icons/logo.png" alt="logo" width={24} height={24} />
-              <p>DevEvents</p>
-            </Link>
-
-            <ul>
-              <Link href="/">Home</Link>
-              <Link href="/dashboard">Dashboard</Link>
-              <Link href="/dashboard/create-event" prefetch={false}>
-                Create Event
-              </Link>
-            </ul>
-          </nav>
-        </header>
-      }
-    >
-      <Navbar />
-    </Suspense>
-  );
-}
-
-export default SuspenseWrapper;
